de2fe265e87629f4324bd5f543e5ddfb3a8f325d
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-01-13 14:44:16 -0300

    fix: correct server label from 'Indiky Web' to 'Uxvision Web'

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,7 @@ export const SERVER_OPTIONS: Array<IServerOption> = [
   { label: 'Indiky Server', value: '3001' },
   { label: 'Shorten', value: '3002' },
   { label: 'Nexus', value: '7000' },
-  { label: 'Indiky Web', value: '4000' },
+  { label: 'Uxvision Web', value: '4000' },
   { label: 'Auto X', value: '4010' },
 ]
 
