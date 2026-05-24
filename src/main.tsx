e2ada456b6671538a7082c95894568ab9382f89f
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-05-23 22:05:27 -0300

    feat: add vieira analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { VieiraAnalytics } from '@vieira/analytics/react'
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <VieiraAnalytics projectKey="serveruler-client" />
   </React.StrictMode>
 )
